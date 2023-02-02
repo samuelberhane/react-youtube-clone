81e0ccf9b0b88b828d540a6643325b3d3c6f205d
samuelberhane <1samibrhane@gmail.com>
2023-02-02 19:56:58 +0300

    Create a Video comment section UI.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { logo } from "../utils/variables";
 import { useNavigate, Link } from "react-router-dom";
 import { BiSearchAlt, BiLogInCircle } from "react-icons/bi";
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../firebase/config";
 
 const Navbar = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [user, setUser] = useState(null);
   const navigate = useNavigate();
+  const provider = new GoogleAuthProvider();
+
+  // handle search
   const handleSubmit = (e) => {
     e.preventDefault();
     if (searchQuery) {
@@ -13,6 +24,16 @@ const Navbar = () => {
     }
     setSearchQuery("");
   };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        setUser(user);
+      }
+    });
+  }, []);
+
   return (
     <div className=" fixed top-0 left-0 w-full h-[70px] shadow-md z-[100] bg-white">
       <div className="flex items-center px-1 py-2 sm:px-8 justify-between h-full">
@@ -44,9 +65,21 @@ const Navbar = () => {
               <BiSearchAlt />
             </button>
           </form>
-          <Link to="/auth/login">
-            <BiLogInCircle className="bg-[#c22113] text-white rounded-full text-4xl p-2" />
-          </Link>
+          <div>
+            {!user ? (
+              <BiLogInCircle
+                className="bg-[#c22113] text-white rounded-full text-4xl p-2 cursor-pointer"
+                onClick={() => signInWithPopup(auth, provider)}
+              />
+            ) : (
+              <img
+                src={user?.photoURL}
+                alt="userProfile"
+                className="w-9 h-9 rounded-full"
+                onClick={() => signOut(auth)}
+              />
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/pages/SingleVideo.jsx
+++ src/pages/SingleVideo.jsx
@@ -6,11 +6,16 @@ import { Navbar, Videos } from "../components";
 import { fetchRelatedData } from "../utils/relatedVideos";
 import { BsFillPatchCheckFill } from "react-icons/bs";
 import { demoChannelTitle, demoChannelUrl } from "../utils/variables";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase/config";
+import { AiOutlineLike, AiOutlineDislike } from "react-icons/ai";
 
 const SingleVideo = () => {
   const { id } = useParams();
   const [videoDetails, setVideoDetails] = useState({});
+  const [user, setUser] = useState(null);
   const [videos, setVideos] = useState([]);
+  const [text, setText] = useState("");
 
   // fetch single video data
   useEffect(() => {
@@ -25,6 +30,21 @@ const SingleVideo = () => {
       setVideos(response.items);
     });
   }, [id]);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        setUser(user);
+      }
+    });
+  }, []);
+
+  // handle comment submit
+  const handleComment = (e) => {
+    e.preventDefault();
+    setText(null);
+  };
 
   // return loading while fetch data
   if (!videoDetails?.snippet) return;
@@ -74,6 +94,131 @@ const SingleVideo = () => {
                 <p className="text-gray-500 text-sm">
                   {parseInt(likeCount).toLocaleString()} likes
                 </p>
+              </div>
+            </div>
+
+            {/* Video comment */}
+            <div className="mb-8">
+              <h1 className="font-bold text-xl mb-3">12 Comments</h1>
+              {user ? (
+                <div>
+                  <div className="flex gap-2">
+                    <img
+                      src={user?.photoURL}
+                      alt="userProfile"
+                      className="w-9 h-9 rounded-full"
+                    />
+                    <form onSubmit={handleComment} className="flex-grow">
+                      <input
+                        type="text"
+                        placeholder="Add a comment..."
+                        className="outline-none w-full border-b-2 py-1 px-4"
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                      />
+                    </form>
+                  </div>
+                  <div className="flex justify-end mt-1">
+                    <button
+                      className="bg-blue-800 text-white font-semibold px-3 py-1 rounded-2xl"
+                      type="submit"
+                      disabled={text.length === 0 ? true : false}
+                    >
+                      Comment
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                <p>Sign in to comment.</p>
+              )}
+              <div className="max-w-[600px] mt-4">
+                <div className="flex gap-4 mb-3">
+                  <img
+                    src={user?.photoURL}
+                    alt="userProfile"
+                    className="w-9 h-9 rounded-full"
+                  />
+                  <div>
+                    <div className="flex items-center gap-1">
+                      <p className="font-bold">Samuel Brhane</p>
+                      <p className="text-[13px] font-light mt-[0.1rem] text-gray-500">
+                        12 days ago
+                      </p>
+                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Quo, rerum.
+                    </p>
+                    <div className="flex items-center gap-4 mt-1">
+                      <div className="flex items-center gap-1">
+                        <AiOutlineLike className="text-lg" />{" "}
+                        <p className="text-sm">2</p>
+                      </div>
+                      <div className="flex items-center gap-1">
+                        <AiOutlineDislike className="text-lg" />{" "}
+                        <p className="text-sm">2</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex gap-4 mb-3">
+                  <img
+                    src={user?.photoURL}
+                    alt="userProfile"
+                    className="w-9 h-9 rounded-full"
+                  />
+                  <div>
+                    <div className="flex items-center gap-1">
+                      <p className="font-bold">Samuel Brhane</p>
+                      <p className="text-[13px] font-light mt-[0.1rem] text-gray-500">
+                        12 days ago
+                      </p>
+                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Quo, rerum.
+                    </p>
+                    <div className="flex items-center gap-4 mt-1">
+                      <div className="flex items-center gap-1">
+                        <AiOutlineLike className="text-lg" />{" "}
+                        <p className="text-sm">2</p>
+                      </div>
+                      <div className="flex items-center gap-1">
+                        <AiOutlineDislike className="text-lg" />{" "}
+                        <p className="text-sm">2</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex gap-4 mb-3">
+                  <img
+                    src={user?.photoURL}
+                    alt="userProfile"
+                    className="w-9 h-9 rounded-full"
+                  />
+                  <div>
+                    <div className="flex items-center gap-1">
+                      <p className="font-bold">Samuel Brhane</p>
+                      <p className="text-[13px] font-light mt-[0.1rem] text-gray-500">
+                        12 days ago
+                      </p>
+                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Quo, rerum.
+                    </p>
+                    <div className="flex items-center gap-4 mt-1">
+                      <div className="flex items-center gap-1">
+                        <AiOutlineLike className="text-lg" />{" "}
+                        <p className="text-sm">2</p>
+                      </div>
+                      <div className="flex items-center gap-1">
+                        <AiOutlineDislike className="text-lg" />{" "}
+                        <p className="text-sm">2</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
