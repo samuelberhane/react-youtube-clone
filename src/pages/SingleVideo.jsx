b4b1af6bad327900f58339b509a62b85c53c96c1
samuelberhane <1samibrhane@gmail.com>
2023-02-02 11:07:42 +0300

    Install  react-icons and update SingleVideo page contents.

--- src/pages/SingleVideo.jsx
+++ src/pages/SingleVideo.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Stack, Box, Typography } from "@mui/material";
 import ReactPlayer from "react-player";
 import { fetchSingleVideo } from "../utils/singleVideoData";
 import { useParams, Link } from "react-router-dom";
 import { Videos } from "../components";
 import { fetchRelatedData } from "../utils/relatedVideos";
-import { CheckCircle } from "@mui/icons-material";
+import { BsFillPatchCheckFill } from "react-icons/bs";
 
 const SingleVideo = () => {
   const { id } = useParams();
@@ -40,67 +39,50 @@ const SingleVideo = () => {
   } = videoDetails;
 
   return (
-    <Box minHeight="95vh">
-      <Stack direction={{ sm: "column", md: "row" }} mt="20px" mx="20px">
+    <section className="min-h-[95vh]">
+      <div className="flex flex-col md:flex-row mt-5 mx-5">
         {/* playing video description */}
-        <Box flex={1}>
-          {/* video player */}
-          <Box sx={{ width: "100%", marginBottom: "10px" }}>
+        <div className="flex-grow">
+          <div className="w-full mb-3">
             <ReactPlayer
               className="react-player"
               url={`https://www.youtube.com/watch?v=${id}`}
               controls
             />
-          </Box>
-          {/* video player */}
+          </div>
 
-          {/* video tititle */}
-          <Typography
-            variant={{ xs: "subtitle3", sm: "subtitle2", md: "subtitle1" }}
-          >
-            {title}
-          </Typography>
           {/* video title */}
+          <h1 className="font-semibold text-sm sm:text-[16px] md:text-lg">
+            {title}
+          </h1>
 
           {/* details about playing video */}
-          <Stack
-            marginTop="4px"
-            marginBottom="20px"
-            direction="row"
-            alignItems="center"
-            justifyContent="space-between"
-          >
+          <div className="mt-1 mb-5 flex items-center justify-between">
             {/* channel name */}
             <Link to={`/channel/${channelId}`}>
-              <Typography variant="subtitle3">
-                {channelTitle}{" "}
-                <CheckCircle sx={{ fontSize: "12px", ml: "5px" }} />
-              </Typography>
+              <h1 className="font-semibold text-lg">
+                {channelTitle} <BsFillPatchCheckFill className="text-sm ml-1" />
+              </h1>
             </Link>
-            {/* channel name */}
 
             {/*  video statistics*/}
-            <Stack direction="row" gap="10px" alignItems="center">
-              <Typography sx={{ color: "grey", fontSize: "14px" }}>
+            <div className="flex gap-2 items-center">
+              <p className="text-gray-500 text-sm">
                 {parseInt(viewCount).toLocaleString()} views
-              </Typography>
-              <Typography sx={{ color: "grey", fontSize: "14px" }}>
+              </p>
+              <p className="text-gray-500 text-sm">
                 {parseInt(likeCount).toLocaleString()} likes
-              </Typography>
-            </Stack>
-            {/* video statistics */}
-          </Stack>
-          {/* details about playing video */}
-        </Box>
-        {/* playing video description */}
+              </p>
+            </div>
+          </div>
+        </div>
 
         {/* related videos */}
-        <Box sx={{ mx: "15px" }}>
-          <Videos videos={videos} direction="column" />
-        </Box>
-        {/* related videos */}
-      </Stack>
-    </Box>
+        <div className="mx-3">
+          <Videos videos={videos} className="flex flex-col" />
+        </div>
+      </div>
+    </section>
   );
 };
 
