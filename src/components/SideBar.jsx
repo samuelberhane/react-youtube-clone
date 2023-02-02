72499b0b74226d1135430f9b2a8c93ed62231ada
samuelberhane <1samibrhane@gmail.com>
2023-02-02 11:26:22 +0300

    Update SideBar component.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,33 +1,25 @@
 import React from "react";
-import { Stack } from "@mui/material";
 import { categories } from "../utils/variables";
 
 const SideBar = ({ currentCategory, setCurrentCategory }) => {
   return (
-    <Stack
-      direction="row"
-      sx={{
-        overflowY: "auto",
-        height: { sx: "auto", md: "95%" },
-        flexDirection: { md: "column" },
-      }}
-    >
+    <div className="flex overflow-auto h-auto md:h-[95%] md:flex-col">
       {categories.map((category, index) => {
         const { name, icon } = category;
         return (
           <button
             onClick={() => setCurrentCategory(name)}
             key={index}
-            className="category-btn"
-            style={{
-              backgroundColor: name === currentCategory && "rgb(245, 70, 172)",
-            }}
+            className={`category-btn ${
+              name === backgroundColor && "bg-[rgb(245, 70, 172)]"
+            }`}
           >
             <span
-              style={{
-                color: name === currentCategory ? "#000" : "rgb(45, 70, 92)",
-                marginRight: "10px",
-              }}
+              className={`mt-2 ${
+                name === currentCategory
+                  ? "text-black"
+                  : "text-[rgb(45, 70, 92)]"
+              }`}
             >
               {icon}
             </span>
@@ -35,7 +27,7 @@ const SideBar = ({ currentCategory, setCurrentCategory }) => {
           </button>
         );
       })}
-    </Stack>
+    </div>
   );
 };
 
