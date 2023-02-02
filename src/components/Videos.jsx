5177db7a8f0e741dac68fce629c4185dab111327
samuelberhane <1samibrhane@gmail.com>
2023-02-02 11:35:38 +0300

    Update Videos component.

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { VideoCard } from "./";
-import { Box } from "@mui/material";
 
 const Videos = ({ videos, direction }) => {
   if (!videos?.length)
@@ -10,26 +9,14 @@ const Videos = ({ videos, direction }) => {
       </h1>
     );
   return (
-    <Box
-      sx={{
-        flexDirection: {
-          md: `${direction || "row"}`,
-          sx: "row",
-        },
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: "30px",
-        flexWrap: "wrap",
-        px: "10px",
-      }}
+    <div
+      className={`flex w-full items-center justify-center gap-7 flex-wrap  px-2`}
     >
       {/* map all videos data */}
       {videos.map((item, index) => {
         return <VideoCard video={item} key={index} />;
       })}
-    </Box>
+    </div>
   );
 };
 
