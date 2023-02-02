5417edcdb20eacdc48bd1e70b423d405474addec
samuelberhane <1samibrhane@gmail.com>
2023-02-02 11:17:49 +0300

    Update Navbar component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,16 @@
 import React from "react";
-import Stack from "@mui/material/Stack";
 import { Link } from "react-router-dom";
 import { logo } from "../utils/variables";
-
-import { SearchBar } from "../components";
+import { SearchBar } from ".";
 
 const Navbar = () => {
   return (
-    <Stack
-      direction="row"
-      alignItems="center"
-      sx={{
-        px: 3,
-        py: 2,
-        backgroundColor: "#fff",
-        justifyContent: "space-between",
-        position: "sticky",
-        top: 0,
-        boxShadow: "1px 2px 4px",
-      }}
-    >
-      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+    <div className="flex items-center px-3 py-2 bg-white justify-between sticky top-0 shadow-md">
+      <Link to="/" className="flex items-center">
         <img src={logo} alt="Youtube logo" height={55} />
       </Link>
       <SearchBar />
-    </Stack>
+    </div>
   );
 };
 
