5f49c015e33de28dc9ce55571a0c5e0b4acdd04e
samuelberhane <1samibrhane@gmail.com>
2023-02-02 10:53:32 +0300

    Update SingleChannel page contents.

--- src/pages/SingleChannel.jsx
+++ src/pages/SingleChannel.jsx
@@ -3,12 +3,12 @@ import { fetchChannelData } from "../utils/channelData";
 import { fetchChannelVideos } from "../utils/channelVideos";
 import { useParams } from "react-router-dom";
 import { ChannelCard, VideoCard } from "../components";
-import { Stack, Box } from "@mui/material";
 
 const SingleChannel = () => {
   const { id } = useParams();
   const [channelDetails, setChannelDetails] = useState([]);
   const [channelVideos, setChannelVideos] = useState([]);
+
   // fetch data
   useEffect(() => {
     fetchChannelData(id).then((data) => {
@@ -23,43 +23,23 @@ const SingleChannel = () => {
   }, [id]);
 
   return (
-    <Stack>
+    <section>
       {/* background div */}
-      <Box
-        sx={{
-          zIndex: "-10",
-          height: "160px",
-          background:
-            "linear-gradient(45deg,rgb(124,34,112) 25%,rgb(123,45,221) 50%,rgb(23,195,121) 25%)",
-        }}
-      ></Box>
-      {/* background div */}
+      <div className="-z-10 w-full h-[160px] bg-gradient-to-tl from-green-300 via-red-400 to-yellow-300 "></div>
 
       {/* channel card */}
       {channelDetails.map((channel, index) => {
         return <ChannelCard key={index} channel={channel} />;
       })}
-      {/* channel card */}
 
       {/* channel videos */}
-      <div
-        style={{
-          width: "100%",
-          marginTop: "240px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "22px",
-          flexWrap: "wrap",
-          px: "10px",
-        }}
-      >
+      <div className="w-full mt-[240px] flex items-center justify-center gap-[22px] flex-wrap px-[10px]">
         {channelVideos.map((video, index) => {
           return <VideoCard video={video} key={index} />;
         })}
       </div>
       {/* channel videos */}
-    </Stack>
+    </section>
   );
 };
 
